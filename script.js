d6c044420c6a12c625407532cbd0857970186867
JonoHasMono <fry22331@lcsd2kids.org>
2021-04-12 11:00:55 -0600

    Finished some elements on the page, started working on the coding for the game

--- script.js
+++ script.js
@@ -1,43 +1,24 @@
 const choices = ["rock", "paper", "scissors"];
 const questionPlayer = `Rock, paper, or scissors?`;
+
+function singleGame(playerChoice, computerChoice) {
+    let playerChoice = ""
+    let computerChoice = Math.floor(Math.random() * choices.length);
+}
+
 //let playerAnswer = prompt(questionPlayer);
 
-function startGame() {
+
+function rockClicked() {
+    playerChoice = "Rock"
+    alert("You chose: Rock.")
+}
 
 
-    // Makes the player choose what to play: Rock, paper, or scissors
-    function playerSelection() {
-
-        let playerAnswerLC = playerAnswer.toLowerCase();
-        
-        if(playerAnswerLC == 'rock') {
-            alert('You chose: Rock.');  
-        } else if(playerAnswerLC == 'paper') {
-         alert('You chose: Paper.');
-        } else if(playerAnswerLC == 'scissors') {
-         alert('You chose: Scissors.');
-        } else {
-         alert('Nah bro, you gotta put rock, paper, or scissors');
-        }
-            
-    }
-
-    playerSelection();
 
     // Causes the computer to make a random choice
     function computerPlay() {
         const randomChoice = Math.floor(Math.random() * choices.length);
     
-       // alert(choices[randomChoice]);
+       alert(choices[randomChoice]);
     }
-
-
-    // Executes the code of the computerPlay function
-    computerPlay();
-    
-}
-
-
-
-
-startGame(); 
