a558a248fb768ac8f691c05d95a2766a81e52da8
JonoHasMono <fry22331@lcsd2kids.org>
2021-04-13 08:51:11 -0600

    Trying to work on the code for the computer

--- script.js
+++ script.js
@@ -1,28 +1,36 @@
 let playerChoice = "";
 let computerChoice = "";
 function computerPlay() {
-    let randomNum = Math.ceil(Math.random()*3);
-    if(randomNum == 1){
-        computerChoice = "Rock";
-    } else if(randomnum == 2) {
-        computerChoice = "Paper";
+    let randomNum = Math.ceil(Math.random() * 3);
+    if(randomNum == 1) {
+        let computerChoice = "Rock"
+    } else if(randomNum == 2) {
+        let computerChoice = "Paper"
     } else {
-        computerChoice = "Scissors";
+        let computerChoice = "Scissors"
     }
 }
 
 
 function singleGame() {
+    computerPlay();
     alert("Computer chose: " + computerChoice);
     if(playerChoice == computerChoice) {
         alert("You guys tied? Lame.");
+        computerChoice = "";
     } else {
         alert("Nice");
+        computerChoice = "";
     }
 }
 
 
 function rockClicked() {
     playerChoice = "Rock";
+    singleGame();
+}
+
+function paperClicked() {
+    playerChoice = "Paper";
     singleGame();
 }
