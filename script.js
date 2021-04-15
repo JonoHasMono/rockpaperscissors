2cb28dad4f6817674f4f29bff2073fda5d4488dd
JonoHasMono <fry22331@lcsd2kids.org>
2021-04-15 09:31:16 -0600

    Added a luck feature to the game, I plan to add a shop in the bottom corner that allows you to buy something to increase your luck.

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let playerChoice = "";
 let computerChoice = "";
+let score = 0
+let bonusPoints = 1
+let bonusChance = 0.25
 function computerPlay() {
     let randomNum = Math.ceil(Math.random() * 3)
     if(randomNum == 1) {
@@ -14,71 +17,137 @@ function computerPlay() {
     }
 }
 
+function bonusChanceRoll() {
+    let randomBonus = Math.random()
+    if(bonusChance > randomBonus) {
+        document.getElementById("score").innerHTML = freePoint();
+        document.getElementById("freePoint").innerHTML = freePointText();
+    } else {
+        document.getElementById("freePoint").innerHTML = "";
+    }
+}
+
+function freePoint() {
+    return score = score + 1
+}
+
+function freePointText() {
+    return "Lucky!"
+}
 
 function singleGame() {
     computerPlay();
+    bonusChanceRoll();
+
     if(playerChoice == "Glock") {
         document.getElementById("demo").innerHTML = glockOutcomes();
+        document.getElementById("score").innerHTML = glockScores();
     } else if(playerChoice == "Rock") {
         document.getElementById("demo").innerHTML =  rockOutcomes();
+        document.getElementById("score").innerHTML = rockScores();
     } else if(playerChoice == "Paper") {
         document.getElementById("demo").innerHTML =  paperOutcomes();
+        document.getElementById("score").innerHTML = paperScores();
     } else {
         document.getElementById("demo").innerHTML = scissorsOutcomes();
+        document.getElementById("score").innerHTML = scissorsScores();
     }
 }
 
 function rockOutcomes() {
     if(computerChoice == "Rock") {
-        
-        
-        
-    return "Opponent chose Rock, you tied";
+    return "You tied ";
     } else if(computerChoice == "Paper") {
-        
-    return "Opponent chose Paper, you lost";
+    return "You lost";
     } else {
-        
-    return "Opponent chose Scissors, you won";
+    return "You won";
     }
     computerChoice = ""
     playerChoice = ""
+}
+
+function rockScores() {
+    if(computerChoice == "Rock") {
+    return score + 0;
+    } else if(computerChoice == "Paper") {
+        if(score == 0) {
+            return score = score + 0;
+             } else if(score < 0) {
+                 return score = score * 0;
+             }
+    return score = score - 1;
+    } else {
+    return score = score + bonusPoints;
+    }
 }
 
 function paperOutcomes() {
     if(computerChoice == "Paper") {
         
-    return "Opponent chose Paper, you tied";
+    return "You tied";
     } else if(computerChoice == "Scissors") {
         
-    return "Opponent chose Scissors, you lost";
+    return "You lost";
     } else {
         
-    return "Opponent chose Rock, you won";
+    return "You won";
     }
     computerChoice = ""
     playerChoice = ""
 }
 
+function paperScores() {
+    if(computerChoice == "Paper") {
+    return score + 0;
+    } else if(computerChoice == "Scissors") {
+        if(score == 0) {
+            return score = score + 0;
+             } else if(score < 0) {
+                 return score = score * 0;
+             }
+    return score = score - 1;
+    } else {
+    return score = score + bonusPoints;
+    }
+}
+
 function scissorsOutcomes() {
     if(computerChoice == "Scissors") {
-        
-    return "Opponent chose Scissors, you tied";
+    return "You tied";
     } else if(computerChoice == "Rock") {
         
-    return "Opponent chose Rock, you lost";
+    return "You lost";
     } else {
         
-    return "Opponent chose Paper, you won";
+    return "You won";
     }
     computerChoice = ""
     playerChoice = ""
+}
+
+function scissorsScores() {
+    if(computerChoice == "Scissors") {
+    return score + 0;
+    } else if(computerChoice == "Rock") {
+        if(score == 0) {
+            return score = score + 0;
+             } else if(score < 0) {
+                 return score = score * 0;
+             }
+    return score = score - 1;
+    } else {
+    return score = score + bonusPoints;
+    }
 }
 
 function glockOutcomes() {
     return "You shot your opponent 8 times, you won";
     computerChoice = ""
     playerChoice = ""
+}
+
+function glockScores() {
+    return score = score + 1;
 }
 
 
